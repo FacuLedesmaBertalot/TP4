4da7422e1eab754ac735ef2d2d2630733175a388
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-09-20 19:25:42 -0300

    Añadido Structure/footer.php

---
+++ View/Structure/footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Footer</title>
+  <link rel="stylesheet" href="../Frameworks/bootstrap.min.css">
+</head>
+  <!-- Footer -->
+  <footer class="bg-primary text-center text-white py-3 mt-auto shadow">
+    <small>&copy; 2025 PWD - Todos los derechos reservados</small>
+  </footer>
+</html>
