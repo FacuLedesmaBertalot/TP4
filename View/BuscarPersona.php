5fc6bc5ac0773e93049e5f5ae191b40f59335e32
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-09-21 14:43:01 -0300

    Inicializado BuscarPersona.php

---
+++ View/BuscarPersona.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buscar Persona</title>
+    <link rel="stylesheet" href="Frameworks/bootstrap.min.css">
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+
+    <?php require "../View/Structure/header.php"; ?>
+
+    <div class="container mt-5">
+        <form action="accionBuscarPersona.php" method="post" onsubmit="return validarBuscarPersona()">
+            <div class="mb-3">
+                <label for="dni" class="form-label">Número de Documento:</label>
+                <input type="text" name="dni" id="dni" class="form-control" required>
+            </div>
+            <button type="submit" class="btn brn-primary">Buscar</button>
+        </form>
+
+    </div>
+
+    <?php require "../View/Structure/footer.php" ?>
+
+
+    <script src="Frameworks/bootstrap.bundle.min.js"></script>
+    <script src="Frameworks/js/validarFormulario.js"></script>
+
+</body>
+</html>
