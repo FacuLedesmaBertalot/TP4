1fdd214656f8e1b80649f6a7d141c6590c989cff
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-09-25 11:39:34 -0300

    Cambios

--- View/CambioDuenio.php
+++ View/CambioDuenio.php
@@ -10,10 +10,11 @@
 
 <body class="d-flex flex-column min-vh-100">
 
-    <?php require "../View/Structure/header.php" ?>
+    <?php require "../View/Structure/header.php"; ?>
 
     <div class="container mt-5">
-        <h2>Cambio de Dueño de Auto</h2>
+        <h2 class="mb-4">Cambio de Dueño de Auto</h2>
+
         <form action="accionCambioDuenio.php" method="post" onsubmit="return validarFormularioCambio()">
             <div class="mb-3">
                 <label for="patente" class="form-label">Patente del Auto:</label>
@@ -23,16 +24,19 @@
                 <label for="dniDuenio" class="form-label">DNI del Nuevo Dueño:</label>
                 <input type="text" class="form-control" id="dniDuenio" name="dniDuenio" required>
             </div>
-            <button type="submit" class="btn btn-primary">Cambiar Dueño</button>
+
+            <div class="d-flex justify-content-start gap-2">
+                <button type="submit" class="btn btn-primary">Cambiar Dueño</button>
+                <a href="../index.php" class="btn btn-primary">Volver</a>
+            </div>
         </form>
     </div>
 
-    <?php require "../View/Structure/footer.php" ?>
+    <?php require "../View/Structure/footer.php"; ?>
 
     <script src="Frameworks/bootstrap.bundle.min.js"></script>
     <script src="Frameworks/js/validarFormulario.js"></script>
 
-    
 </body>
-
 </html>
+
