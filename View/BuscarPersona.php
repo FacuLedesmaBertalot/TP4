5b8b1c142ad5c3d4a2f34ccd7531ae2629da65b6
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-09-25 11:42:32 -0300

    Cambios en rutas para index.php

--- View/BuscarPersona.php
+++ View/BuscarPersona.php
@@ -13,18 +13,31 @@
     <?php require "../View/Structure/header.php"; ?>
 
     <div class="container mt-5">
-        <form action="accionBuscarPersona.php" method="post" onsubmit="return validarBuscarPersona()">
-            <div class="mb-3">
-                <label for="dni" class="form-label">Número de Documento:</label>
-                <input type="text" name="dni" id="dni" class="form-control" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Buscar</button>
-        </form>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card shadow-lg">
+                    <div class="card-header bg-primary text-white text-center">
+                        <h4>Buscar Persona</h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="accionBuscarPersona.php" method="post" onsubmit="return validarBuscarPersona()">
+                            <div class="mb-3">
+                                <label for="dni" class="form-label">Número de Documento:</label>
+                                <input type="text" name="dni" id="dni" class="form-control" required>
+                            </div>
 
+                            <div class="d-flex justify-content-start gap-2">
+                                <button type="submit" class="btn btn-primary">Buscar</button>
+                                <a href="../index.php" class="btn btn-primary">Volver</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <?php require "../View/Structure/footer.php" ?>
-
+    <?php require "../View/Structure/footer.php"; ?>
 
     <script src="Frameworks/bootstrap.bundle.min.js"></script>
     <script src="Frameworks/js/validarFormulario.js"></script>
