3651c2c6edf0c57a5d29340c1ccfa99bcfaf5301
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-09-26 11:09:51 -0300

    Añadido __DIR__ a AutoControl.php

--- Controllers/AutoControl.php
+++ Controllers/AutoControl.php
@@ -1,6 +1,7 @@
 <?php
-include_once "../Models/Conector/BaseDatos.php";
-include_once "../Models/Auto.php";
+include_once(__DIR__ . "/../Models/Conector/BaseDatos.php");
+include_once(__DIR__ . "/../Models/Auto.php");
+
 
 class AutoControl extends Auto {
 
